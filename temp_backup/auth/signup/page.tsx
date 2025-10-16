fb15c44b4b86db0a65ce990a7b24d852b60fc096
Will Alston <faradaybach@gmail.com>
2025-10-16 11:53:42 -0400

    Final clean: Move backup outside project for successful build

--- temp_backup/auth/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthForm } from '@/components/auth/auth-form'
-
-export default function SignupPage() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-emerald-50 to-gold-50 dark:from-gray-900 dark:to-gray-800 flex items-center justify-center p-6">
-      <AuthForm mode="signup" onModeChange={() => {}} />
-    </div>
-  )
-}
